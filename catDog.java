268a52e9b9f3d192bedaa5723c32b591e5b54741
Kyiesha Butler <13741352+kyiesha@users.noreply.github.com>
2019-07-09 22:06:03 -0400

    Add files via upload

---
+++ catDog.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class catDog {
+	  public static void main(String[] args) {
+	    System.out.println("Please enter Dog or Cat.");
+	    
+	    Scanner input = new Scanner(System.in);
+	     pet = in.nextLine();
+	    if {
+            pet = "Dog";
+            System.out.println("Woof");
+        }
+        else {
+            System.out.println("Meow");
+        }
+
+	}
+}
